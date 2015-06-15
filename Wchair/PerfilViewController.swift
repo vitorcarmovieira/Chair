daf8ed154d67086407af2e70b8c9c466db885cc2
Rodrigo Conte <rodrigo.conte14@gmail.com>
2015-06-15 15:26:06 -0400

    Criação da classe de perfil

---
+++ Wchair/PerfilViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PerfilViewController.swift
+//  Wchair
+//
+//  Created by Rodrigo Conte on 15/06/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+import UIKit
+
+class PerfilViewController: UIViewController {
+
+     @IBOutlet weak var LbName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
