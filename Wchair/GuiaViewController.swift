15d86c84151a40e33ca16530fdb80117cc118cfc
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-15 14:58:37 -0400

    Criado actions da tela guia

---
+++ Wchair/GuiaViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GuiaViewController.swift
+//  Wchair
+//
+//  Created by Vitor on 6/15/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+import UIKit
+
+class GuiaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func exercicios(sender: AnyObject) {
+        
+        
+    }
+
+    
+    @IBAction func esportes(sender: AnyObject) {
+        
+        
+    }
+    @IBAction func beneficios(sender: AnyObject) {
+        
+        
+    }
+    @IBAction func associacoes(sender: AnyObject) {
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
