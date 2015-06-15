0b153a6f5c155c3552d54ccb638f8fb7ea33bba0
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-15 14:51:55 -0400

    Criada tableviewController e arquivos necessários

---
+++ Wchair/GuiaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GuiaTableViewCell.swift
+//  Wchair
+//
+//  Created by Vitor on 6/15/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+import UIKit
+
+class GuiaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
