fe6a04e976bb3046b88b3cbf51bb0e4f65c7c447
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-17 15:00:08 -0400

    adicionado tela que mostra os exercícios

---
+++ Wchair/ExercicioViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ExercicioViewController.swift
+//  Wchair
+//
+//  Created by Vitor on 6/17/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+import UIKit
+
+class ExercicioViewController: UIViewController {
+
+    @IBOutlet weak var exercicioDescription: UITextView!
+    @IBOutlet weak var exercicioImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
