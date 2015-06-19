c8d558002cdd818ba6673c52ab5411428773f5f7
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-19 16:46:11 -0400

    comecei a implementar a page que fica na tela de detalhes do esporte

--- Wchair/ExercicioViewController.swift
+++ Wchair/ExercicioViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ExercicioViewController: UIViewController {
 
     @IBOutlet weak var exercicioDescription: UITextView!
-    @IBOutlet weak var exercicioImage: UIImageView!
+    @IBOutlet weak var imageScroll: UIScrollView!
     
     var item: AnyObject?
     var tag: Int?
@@ -27,7 +27,7 @@ class ExercicioViewController: UIViewController {
         case 1://Exercicio tipo1
             
             let exercicio = self.item as! Exercicio
-            exercicioImage.image = UIImage(data: exercicio.imageGuia)
+//            exercicioImage.image = UIImage(data: exercicio.imageGuia)
             exercicioDescription.text = exercicio.detalhes
             
             self.title = exercicio.nome
@@ -35,7 +35,7 @@ class ExercicioViewController: UIViewController {
         case 2://Exercicio tipo2
             
             let exercicio = self.item as! Exercicio
-            exercicioImage.image = UIImage(data: exercicio.imageGuia)
+//            exercicioImage.image = UIImage(data: exercicio.imageGuia)
             exercicioDescription.text = exercicio.detalhes
             
             self.title = exercicio.nome
@@ -43,7 +43,7 @@ class ExercicioViewController: UIViewController {
         case 3://Exercicio tipo3
             
             let exercicio = self.item as! Exercicio
-            exercicioImage.image = UIImage(data: exercicio.imageGuia)
+//            exercicioImage.image = UIImage(data: exercicio.imageGuia)
             exercicioDescription.text = exercicio.detalhes
             
             self.title = exercicio.nome
@@ -51,7 +51,7 @@ class ExercicioViewController: UIViewController {
         case 4://Exercicio tipo4
             
             let exercicio = self.item as! Exercicio
-            exercicioImage.image = UIImage(data: exercicio.imageGuia)
+//            exercicioImage.image = UIImage(data: exercicio.imageGuia)
             exercicioDescription.text = exercicio.detalhes
             
             self.title = exercicio.nome
@@ -59,8 +59,23 @@ class ExercicioViewController: UIViewController {
         case 5://ESPORTES
             
             let esporte = self.item as! Esporte
-            exercicioImage.image = UIImage(data: esporte.foto)
             exercicioDescription.text = esporte.descricao
+            
+            var colors:[UIColor] = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor()]
+            var frame: CGRect = CGRectMake(0, 0, 0, 0)
+            
+            for index in 0..<colors.count {
+                
+                frame.origin.x = self.imageScroll.frame.size.width * CGFloat(index)
+                frame.size = self.imageScroll.frame.size
+                self.imageScroll.pagingEnabled = true
+                
+                var subView = UIView(frame: frame)
+                subView.backgroundColor = colors[index]
+                self.imageScroll.addSubview(subView)
+            }
+            
+            self.imageScroll.contentSize = CGSizeMake(self.imageScroll.frame.size.width * CGFloat(colors.count), self.imageScroll.frame.size.height)
             
             self.title = esporte.modalidade
             
