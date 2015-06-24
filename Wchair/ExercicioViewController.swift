4cce3f79d3c494aeb692a20c6e89064d84ee8c07
vitorcarmovieira <vitorcarmovieira@gmail.com>
2015-06-24 13:41:33 -0400

    Cronometro adicionado

--- Wchair/ExercicioViewController.swift
+++ Wchair/ExercicioViewController.swift
@@ -12,12 +12,72 @@ class ExercicioViewController: UIViewController {
     
     var exercicio: Exercicio?
     
+    @IBOutlet weak var iniciarButton: UIButton!
+    @IBOutlet weak var cronometroLabel: UILabel!
+    
+    var startTime = NSTimeInterval()
+    var timer = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
 
+    @IBAction func iniciar(sender: AnyObject) {
+        
+        let nome = iniciarButton.titleLabel?.text
+        
+        if nome! == "Iniciar"{
+            
+            let aSelector : Selector = "updateTime"
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target:self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+            
+            iniciarButton.setTitle("Parar", forState: UIControlState.Normal)
+        } else{
+            
+            timer.invalidate()
+            timer = NSTimer()
+        }
+    }
+    
+    func updateTime() {
+        
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        
+        cronometroLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
